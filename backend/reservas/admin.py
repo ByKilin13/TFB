2b17fe3a638a162176fd6bc5477c0b1845bc686a
ByKilin13 <niconicolaskg@gmail.com>
2026-07-22 01:13:35 +0200

    primeros espacios creados

--- backend/reservas/admin.py
+++ backend/reservas/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Espacio
+
+@admin.register(Espacio)
+class EspacioAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "capacidad",
+        "localizacion",
+        "activo"
+    )
 
 # Register your models here.
--- backend/reservas/models.py
+++ backend/reservas/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
-class Espacios(models.Model):
-    nombre = models.CharField(min_length=2, max_length=100)
+class Espacio(models.Model):
+    nombre = models.CharField(max_length=100)
     descripcion = models.TextField(blank=True)
     capacidad = models.PositiveIntegerField(default=1) 
     localizacion = models.CharField(max_length=180, blank=True)   
     activo = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.nombre
