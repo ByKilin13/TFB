eafdef3086f2e75f3e7d317ef53dd65900164f24
ByKilin13 <niconicolaskg@gmail.com>
2026-07-22 18:05:43 +0200

    nueva clase reserva

--- backend/reservas/admin.py
+++ backend/reservas/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Espacio
+from .models import Espacio, Reserva
 
 @admin.register(Espacio)
 class EspacioAdmin(admin.ModelAdmin):
@@ -8,6 +8,22 @@ class EspacioAdmin(admin.ModelAdmin):
         "capacidad",
         "localizacion",
         "activo"
+    )
+
+@admin.register(Reserva)
+class ReservaAdmin(admin.ModelAdmin):
+    list_display = ( #define las columnas que aparecen en el listado de reservas
+        "espacio",
+        "usuario",
+        "fecha",
+        "hora_inicio",
+        "hora_fin",
+        "estado"
+    )
+    list_filter = ( #filtros para el listado
+        "estado",
+        "fecha",
+        "espacio"
     )
 
 # Register your models here.
--- backend/reservas/models.py
+++ backend/reservas/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.conf import settings  #sirve para relacionar reservas con el usuario
+
+
 
 class Espacio(models.Model):
     nombre = models.CharField(max_length=100)
@@ -9,3 +12,30 @@ class Espacio(models.Model):
 
     def __str__(self):
         return self.nombre
+
+class Reserva(models.Model):
+    estados = [
+        ("activa", "Activa"),
+        ("cancelada", "Cancelada")
+    ]    
+    usuario = models.ForeignKey( #foreignkey indica un usuario a muchas reservas y una reserva un usuario
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT, #impide eliminar usuario si hay reservas en el
+        related_name="reservas" #permite hacer consulta de todas las reservas del usuario
+    )
+    espacio = models.ForeignKey( #foreignkey indica un espacio muchas reservas y una reserva un espacio
+        Espacio, 
+        on_delete=models.PROTECT,
+        related_name="reservas"
+    )
+    fecha = models.DateField()
+    hora_inicio = models.TimeField()
+    hora_fin = models.TimeField()
+    estado = models.CharField(
+        max_length=10,
+        choices= estados,
+        default="activa"
+    )
+    def __str__(self):
+        return f"{self.espacio.nombre} - {self.fecha}"
+
